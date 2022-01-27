43dacc98ebabe084a9fe64e682a66fbfe2cf594e
aster kanouo <aster.kanouo@gmail.com>
2022-01-27 12:50:05 +0100

    ajout du fichier procfile et utilisation de apache2

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/testtere', function () {
+    return view('test');
+})->name('test');
+
+route::get('/a_proposs', function(){
+    return view('about');
+})->name('about');
